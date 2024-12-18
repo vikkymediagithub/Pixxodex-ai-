139ca544761617aefc888bf4016d15be207237f2
VictorJoshuaC <83277865+VictorJoshuaC@users.noreply.github.com>
2024-12-18 10:17:44 +0100

    g

--- js/main.js
+++ js/main.js
@@ -100,3 +100,12 @@ function toggleDropdown(sectionId) {
     icon.textContent = "+"; // Show plus icon
   }
 }
+
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'd98c76709802bdfdc26bd34e7f1755c0d5df969f';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
