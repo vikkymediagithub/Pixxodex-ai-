4644039a5a52d4b3eb084da7e4353556961306d2
Hi, I'm Victor Joseph <okevictor14@gmail.com>
2025-01-02 14:04:54 +0300

    fixng toggle

--- mobile-dashboard/assets/js/script.js
+++ mobile-dashboard/assets/js/script.js
@@ -5,3 +5,16 @@ window.addEventListener('load', () => {
 });
 
 
+const toggleDarkMode = () => {
+    document.documentElement.classList.toggle('dark');
+    localStorage.theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+  };
+  
+  // Apply theme on page load based on localStorage or OS preference
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+  
+
